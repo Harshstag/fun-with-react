caf4378b21437c3bfc43edac1dcac2a8537bf8a5
Harsh Singh <hrsharshsingh@gmail.com>
2024-10-09 19:03:38 +0530

    reactRouter-layout-githubApiOptimization

---
+++ 05reactRouter/src/components/Github/Github.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+import { useLoaderData } from "react-router-dom";
+
+function Github() {
+  //   const [data, setData] = useState([]);
+  //   useEffect(() => {
+  //     fetch("https://api.github.com/users/harshstag")
+  //       .then((response) => response.json())
+  //       .then((data) => {
+  //         console.log(data);
+  //         setData(data);
+  //       });
+  //   }, []);
+
+  const data = useLoaderData(); // optimize way of doing it
+
+  return (
+    <div className="text-ceneter m-4 bg-gray-600 text-white p-4 text-3xl">
+      Github followers: {data.followers}
+      <img src={data.avatar_url} alt="Git picture" width={300} />
+    </div>
+  );
+}
+
+export default Github;
+
+export const githubLoader = async () => {
+  const response = await fetch("https://api.github.com/users/harshstag");
+  return response.json();
+};
+// more optimize way of loading an api data
